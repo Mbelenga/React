9c75c307adf76f3bcd7996e5cad1c282e1330875
Mbelenga <mbelengaterence@gmail.com>
2025-03-07 17:26:07 +0300

    updated

--- My-First-React-Project/src/assets/index.jsx
+++ My-First-React-Project/src/assets/index.jsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client"
-const root =  createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+const reactElement = <h1>Hello from JSX!</h1>
+
+console.log(reactElement)
+
 root.render(
-    <h1>Hello from react!</h1>
+    reactElement
 )
