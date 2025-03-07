8e7a855b232a3fca65b4f1f6aa5a2f932efd69f4
Mbelenga <mbelengaterence@gmail.com>
2025-03-07 15:35:21 +0300

    updated

--- practice.jsx
+++ practice.jsx
@@ -1,3 +1,14 @@
-import { createRoot } from "reacct-dom/client"
-const root = createRoot(document.getElementById("root"))
-root.render(<h1>Hello</h1>)
+/**
+ * Challenge: Set up a React app from scratch again.
+ * This time, try rendering an unordered list with 2-3 list items inside
+ * with why you're excited to be learning React.
+ */
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root")).render(
+    <h1>Why am excited to learn React</h1>
+    <ul>
+        <li>It willmake me be job ready</li>
+        <li>Its very good </li>
+        <li>It will make me make good ui's</li>
+    </ul>
+)
