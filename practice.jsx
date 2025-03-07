477ae1b0c33ce709040a0a4642522e0951d59ea3
Mbelenga <mbelengaterence@gmail.com>
2025-03-07 15:38:06 +0300

    update

--- practice.jsx
+++ practice.jsx
@@ -5,10 +5,9 @@
  */
 import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root")).render(
-    <h1>Why am excited to learn React</h1>
-    <ul>
-        <li>It willmake me be job ready</li>
-        <li>Its very good </li>
-        <li>It will make me make good ui's</li>
-    </ul>
-)
+        <ul>
+            <li>It willmake me be job ready</li>
+            <li>Its very good </li>
+            <li>It will make me make good ui's</li>
+        </ul>
+);
