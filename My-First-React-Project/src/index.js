9369f5a22af524f334ce1436e2c91aa2bd9ce499
Mbelenga <mbelengaterence@gmail.com>
2025-03-22 23:45:32 +0300

    nesting into another coponent

--- My-First-React-Project/src/index.js
+++ My-First-React-Project/src/index.js
@@ -1,5 +1,13 @@
 function MyButton() {
   return(
-    <button> I'm a Button </button>
+    <button> i'm a button </button>
+  );
+}
+export default function MyApp() {
+  return(
+    <div>
+    <h1> Welcome to my App </h1>
+    <MyButton/>
+    </div>
   );
 }
