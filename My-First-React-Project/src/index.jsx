81da765690ae21fd4b09a642b448de4fd43ab4ff
Mbelenga <mbelengaterence@gmail.com>
2025-03-27 16:50:44 +0300

    working well

--- My-First-React-Project/src/index.jsx
+++ My-First-React-Project/src/index.jsx
@@ -6,7 +6,7 @@ root.render(
         <img src="public/react-logo.jpeg" width="40px" alt="React logo" />
         <h1>Fun facts about React!</h1>
         <ul>
-            <li>Was first release in 2013</li>
+            <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
             <li>Has well over 200K stars on GitHub</li>
             <li>Is maintained by Meta</li>
