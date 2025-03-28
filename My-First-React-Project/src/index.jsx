026150cd29aa166ffb634d04270edfd2c1f6a324
Mbelenga <mbelengaterence@gmail.com>
2025-03-28 13:56:39 +0300

    Done

--- My-First-React-Project/src/index.jsx
+++ My-First-React-Project/src/index.jsx
@@ -1,2 +1,17 @@
-import { CreateRoot } from "react-dom/client"
-const root = CreateRoot(document.getElementById('root'))
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+function Page() {
+    return (
+        <ol>
+            <li>React is a popular library, so I will be able to
+            fit in with all the coolest devs out there! 😎</li>
+            <li>I am more likely to get a job as a front end developer
+            if I know React</li>
+        </ol>
+    )
+}
+
+root.render(
+    <Page />
+)
