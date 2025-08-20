a18649418f686dff0622bd4b71eb699174845136
Mbelenga <mbelengaterence@gmail.com>
2025-08-20 12:32:15 +0300

    My Projects

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -2,6 +2,13 @@ const Home = ({ darkMode, setCurrentPage }) => {
     const quickLinks = [
         {
             title: "About Me",
+            description: "A reflection of my journey and motivations that define me as a developer.",
+            icon: User,
+            color: "from-blue-500 to-blue-600",
+            page: "about"
+        },
+        {
+            title: "My Projects",
             description: ""
         }
     ]
