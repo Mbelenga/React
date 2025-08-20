bfc7c2d95d52eb387d3cefddf035698fe43113b9
Mbelenga <mbelengaterence@gmail.com>
2025-08-20 18:11:40 +0300

    Done

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
