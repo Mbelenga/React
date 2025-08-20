5bbde0ec40b2cc1c4b18c8d8aa7fb01bc08141e9
Mbelenga <mbelengaterence@gmail.com>
2025-08-20 15:39:24 +0300

    Projects section category 1

---
+++ my-react-app/src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+const Projects = [
+    {
+        id: 1,
+        title: "BookHub",
+        description: "A web app for book lovers to share, review, rate and discover new books.",
+        category: "Full-stack",
+        image: "#",
+        github: "https://github.com/Mbelenga/Book-Hub",
+    }
+]
