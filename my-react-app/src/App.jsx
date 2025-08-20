d922ff650049238d0480dad0ac615cc8e487f8e5
Mbelenga <mbelengaterence@gmail.com>
2025-08-20 15:12:53 +0300

    updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Skills from "./pages/Skills";
-import Contact from "./pages/Contact";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
