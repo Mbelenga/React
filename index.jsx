5dad267b5acfac0edf0808b47532e4193d8426e0
Mbelenga <mbelengaterence@gmail.com>
2025-08-22 16:31:19 +0300

    Done with the challenge

--- index.jsx
+++ index.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client"
-// 1. Create a root
+
 const root = CreateRoot(document.getElementById("root"))
-// 2. Render some markup to the root
-root.render(<h1>Hello, React!</h1>)
+root.render(<h1>Hello There</h1>)
