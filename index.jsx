a8da03f34cbfceb4e46e9a549376d62874a61b85
Mbelenga <mbelengaterence@gmail.com>
2025-08-24 23:54:52 +0300

    Updated

--- index.jsx
+++ index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/clent"
 const root = document.getElementById("root")
 root.render(
     <li>
-        <ul>Its interesting</ul>
+        <ul>It's interesting</ul>
         <ul>Its about time</ul>
         <ul>I want to be better in Frontend</ul>
     </li>
