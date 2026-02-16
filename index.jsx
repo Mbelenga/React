3ff27799614d7f0dd0297a56667ffea30bdc3b5c
Terrence <138711355+Mbelenga@users.noreply.github.com>
2026-02-16 23:35:40 +0300

    Remove redundant list item from index.jsx

--- index.jsx
+++ index.jsx
@@ -3,7 +3,6 @@ const root = document.getElementById("root")
 root.render(
     <li>
         <ul>It's interesting</ul>
-        <ul>Its about time</ul>
         <ul>I want to be better in Frontend development</ul>
     </li>
 )
