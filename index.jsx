579957bca21fd4227d0123d77184d218d050f3ca
Terrence <138711355+Mbelenga@users.noreply.github.com>
2026-02-16 23:36:29 +0300

    Add new list item

--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ const root = document.getElementById("root")
 root.render(
     <li>
         <ul>It's interesting</ul>
+        <ul>Its about time</ul>
         <ul>I want to be better in Frontend development</ul>
     </li>
 )
